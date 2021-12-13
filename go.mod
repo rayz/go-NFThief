55ab4b51c7bebc253e1606594364c48bdce87b2d
raymond zhao <raymnzd@gmail.com>
2021-12-13 01:09:11 -0500

    init

---
+++ go.mod
@@ -0,0 +1,10 @@
+module go-NFThief
+
+go 1.17
+
+require github.com/manifoldco/promptui v0.9.0
+
+require (
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
+	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
+)
---
+++ main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"hash/fnv"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/manifoldco/promptui"
+)
+
+type asset struct {
+	ImageUrl string `json:"image_url"`
+	Name     string `json:"name"`
+}
+
+type OpenSeaAssetResponse struct {
+	Assets []asset `json:"assets"`
+}
+
+func downloadAssets(slug string, assets []asset) {
+	if _, err := os.Stat(slug); os.IsNotExist(err) {
+		err := os.Mkdir(slug, 0755)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, asset := range assets {
+			res, err := http.Get(asset.ImageUrl)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer res.Body.Close()
+			h := fnv.New64a()
+			h.Write([]byte(asset.ImageUrl))
+			name := fmt.Sprint(h.Sum64())
+			f := fmt.Sprintf("%s/%s", slug, name)
+			fmt.Println(f, "is the path!")
+			out, err := os.Create(f)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer out.Close()
+			_, err = io.Copy(out, res.Body)
+		}
+	} else {
+		fmt.Println(slug, "folder already exists")
+	}
+}
+
+func getAssets(slug string) []asset {
+	url := fmt.Sprintf("https://api.opensea.io/api/v1/assets?order_by=sale_price&order_direction=desc&offset=0&collection=%s&limit=50&", slug)
+
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer res.Body.Close()
+
+	body, _ := ioutil.ReadAll(res.Body)
+
+	osap := OpenSeaAssetResponse{}
+	err = json.Unmarshal(body, &osap)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	return osap.Assets
+}
+
+func main() {
+	for {
+		prompt := promptui.Select{
+			Label: "Select Action",
+			Items: []string{"Download a collection", "Exit"},
+		}
+
+		_, result, err := prompt.Run()
+
+		if err != nil {
+			fmt.Printf("Prompt failed %v\n", err)
+			return
+		}
+
+		switch result {
+		case "Download a collection":
+			fmt.Print("Enter a collection name: ")
+			scanner := bufio.NewScanner(os.Stdin)
+			ok := scanner.Scan()
+			if ok != true {
+				break
+			}
+			collectionName := strings.Join(strings.Split(strings.ToLower(scanner.Text()), " "), "")
+			fmt.Println("Trying to download:", collectionName)
+			assets := getAssets(collectionName)
+			if len(assets) > 0 {
+				fmt.Println("Going to download..")
+				downloadAssets(collectionName, assets)
+			} else {
+				fmt.Println("Collection", collectionName, "does not exist")
+			}
+		case "Exit":
+			os.Exit(0)
+		}
+	}
+
+}
