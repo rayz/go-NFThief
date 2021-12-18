5bc27af3453a1194976eab953506c91e55cf305d
raymond zhao <raymnzd@gmail.com>
2021-12-18 17:27:40 -0500

    refactor project layout

---
+++ cli/NFThief/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"github.com/rayz/go-NFThief/pkg/download"
+	"os"
+
+	"github.com/manifoldco/promptui"
+)
+
+func main() {
+	for {
+		prompt := promptui.Select{
+			Label: "Select Action",
+			Items: []string{"Download a collection", "Download collection of owner", "Exit"},
+		}
+
+		_, result, err := prompt.Run()
+
+		if err != nil {
+			fmt.Printf("Prompt failed %v\n", err)
+			return
+		}
+
+		switch result {
+		case "Download a collection":
+			download.DownloadByCollection()
+		case "Download collection of owner":
+			download.DownloadByOwner()
+		case "Exit":
+			os.Exit(0)
+		}
+	}
+
+}
